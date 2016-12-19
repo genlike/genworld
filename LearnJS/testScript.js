a21f155186dd64ccffdfcac061c6ecf957280604
genlike <ricardoffosilva@gmail.com>
2016-12-19 23:57:37 +0000

    Primeiro Upload 20161219

---
+++ LearnJS/testScript.js
@@ -0,0 +1,16 @@
+/*function mainfunc() {
+
+	var texto = "Why not?";
+	var condition = confirm("Wanna play a game?");
+
+	if (condition) {
+		texto = "Ok faggot";
+	}
+
+}
+document.getElementById('text1').innerHTML = texto;
+*/
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+ctx.strokeRect(50,50,50,50);
